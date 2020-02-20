bbf616fedc156146ff4f917039726d88127e7d80
Anil Vadlapatla <Anil.S.Vadlapatla@kp.org>
2020-02-20 09:24:54 -0800

    updated app module to import admin module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AdminModule } from './admin/admin.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Admin } from './admin/admin.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    AdminModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST || 'localhost',
+      username: process.env.DATABASE_USERNAME || 'postgres',
+      password: process.env.DATABASE_PASSWORD || '',
+      database: process.env.DATABASE_NAME || 'postgres',
+      port: parseInt(process.env.DATABASE_PORT) || 5432,
+      entities: [Admin],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
